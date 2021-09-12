98a9c6571220623766c73c7f86cd4d59ac49259c
awee01 <85651950+awee01@users.noreply.github.com>
2021-09-12 18:39:02 -0400

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
   user: 'root',
   // Your MySQL password
   password: 'redalert29882agh',
-  database: 'election'
+  database: 'company'
 });
 
 module.exports = db;
